c63c3eea89bc424a53d70a8abe47fd8acfa0fa52
August Dahlkvist <AugustDahlkvist@gmail.com>
2024-12-11 19:20:58 +0100

    Fix tests in day 11

--- day11/src/tests.rs
+++ day11/src/tests.rs
@@ -26,17 +26,10 @@ mod tests
         assert_eq!(result, 22);
     }
 
-    // #[test]
-    // fn part_2_test() 
-    // {
-    //     let result = problem::get_num_stones_cheat_sheets(&String::from(TEST_STR), 25);
-    //     assert_eq!(result, 55312);
-    // }
-
-    // #[test]
-    // fn part_2_test() 
-    // {
-    //      let result = problem::total_trail_score_part2(&String::from(TEST_STR));
-    //      assert_eq!(result, 81);
-    // }
+    #[test]
+    fn part_2_test() 
+    {
+        let result = problem::get_num_stones_cheat_sheets(&String::from(TEST_STR), 25);
+        assert_eq!(result, 55312);
+    }
 }
