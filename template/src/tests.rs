45f6117dd7f7be67f4a0bc1e71d299b7fc214458
August Dahlkvist <AugustDahlkvist@gmail.com>
2024-12-15 16:47:39 +0100

    Add template project

---
+++ template/src/tests.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests 
+{
+    use crate::part1;
+    use crate::part2;
+
+    const TEST_STR: &str = r"";
+
+    #[test]
+    fn part_1_test() 
+    {
+        let result = part1::compute_answer(&String::from(TEST_STR));
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn part_2_test() 
+    {
+        let result = part2::compute_answer(&String::from(TEST_STR));
+        assert_eq!(result, 0);
+    }
+}
